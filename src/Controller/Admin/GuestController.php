2611a297f965c5dd54fde870eb49e8c286d35318
Rivier Nicolas <n.rivier@algo-factory.com>
2025-11-14 09:30:12 +0100

    controller guest activate

--- src/Controller/Admin/GuestController.php
+++ src/Controller/Admin/GuestController.php
@@ -11,6 +11,7 @@ use App\Form\UserType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,7 +22,8 @@ class GuestController extends AbstractController
      */
     public function guests(ManagerRegistry $doctrine)
     {
-        $guests = $doctrine->getRepository(User::class)->findBy(['admin' => false]);
+        $guests = $doctrine->getRepository(User::class)->findAll();
+
         return $this->render('admin/guests/index.html.twig', [
             'guests' => $guests
         ]);
@@ -36,7 +38,6 @@ class GuestController extends AbstractController
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
-        // Si le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
             $hashedPassword = $passwordHasher->hashPassword(
                 $user,
@@ -54,13 +55,47 @@ class GuestController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // Redirection après l'ajout
             return $this->redirectToRoute('admin_guest_index');
         }
 
-        // Affichage du formulaire
         return $this->render('/admin/guests/add.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/admin/guest/delete/{id}', name: 'admin_guest_delete', methods: ['GET'])]
+    public function delete(User $user, ManagerRegistry $doctrine): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'L’invité et son contenu associé ont été supprimés avec succès.');
+
+        return $this->redirectToRoute('admin_guest_index');
+    }
+
+    #[Route('/admin/guest/revoke/{id}', name: 'admin_guest_revoke')]
+    public function revoke(User $user, ManagerRegistry $doctrine)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $user->setActive(false);
+        $doctrine->getManager()->flush();
+
+        $this->addFlash('warning', "L’accès de {$user->getName()} a été révoqué.");
+        return $this->redirectToRoute('admin_guest_index');
+    }
+
+    #[Route('/admin/guest/activate/{id}', name: 'admin_guest_activate')]
+    public function activate(User $user, ManagerRegistry $doctrine)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $user->setActive(true);
+        $doctrine->getManager()->flush();
+
+        $this->addFlash('success', "L’accès de {$user->getName()} a été réactivé.");
+        return $this->redirectToRoute('admin_guest_index');
     }
 }
